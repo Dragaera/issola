cb7896c0e456bfe157d4c5886805c5d39b231672
Michael Senn <michael@morrolan.ch>
2018-12-18 21:39:49 +0100

    Prevent exception when tracking users in DMs.

--- lib/issola/user_manager.rb
+++ lib/issola/user_manager.rb
@@ -1,9 +1,15 @@
 module Issola
   class UserManager
     def track(event)
+      current_nick = if event.server
+                       event.author.display_name
+                     else
+                       nil
+                     end
+
       DiscordUser.get_or_create(
         event.author.id.to_s,
-        last_nick: event.author.display_name
+        last_nick: current_nick
       )
     end
   end
